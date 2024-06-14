993c61fa4d7a37e8d39365ae51bdb72e701fb50e
William-HuangWY <8334551will@gmail.com>
2024-06-14 18:00:59 +0800

    Association_Process

--- EAP_AP.py
+++ EAP_AP.py
@@ -3,53 +3,205 @@ import sys
 import threading, time
 from scapy.all import *
 
+def decode_payload(pkt):
+    return {i.split(':')[0].strip():i.split(':')[1].strip() for i in [p.strip() for p in pkt[Raw].load.decode('utf-8').split('|')]}
+
 class EAP_AP():
-    def __init__(self, port: int, dest_port: int) -> None:
-        self.port = port
-        self.dest_port = dest_port
+    def __init__(self, channel: int, security:str = "EAP-TLS") -> None:
+        self.channel = channel
+        self.security = security
+        self.broadcasting = True
+        self.target_sta = {
+            'found': False,
+            'wait_for': False
+        }
+        self.usage = dict()
         self.setup()
 
     def setup(self):
         # Create broadcasting thread
+        self.usage = {
+            'seq_num': 0,
+            # WIFI (simulated)
+            'AP_SSID': "EAP_AP",
+            'AP_CHANNEL': self.channel,
+            'AP_RATE': 1,
+            # ETHERNET
+            'port': 1000 + self.channel - 1,
+            'dst_port': 1000 + self.channel,
+        }
+        print(f"AP<{get_if_addr(conf.iface)}:{self.usage['port']}> Setup", '\n')
         self.broadcast_thread = threading.Thread(target=lambda: self.broadcast_beacon(interval=3), daemon=True)
         self.broadcast_thread.start()
-        print(f"AP<{get_if_addr(conf.iface)}:{self.port}> Setup", '\n')
     
     def start(self):
         # Start broadcasting thread
-        print(f"AP start boardcasting at: {self.dest_port}", '\n')
-        
-        # while True:
-        #     self.handle_signal()
+        print(f"AP start Boardcasting at Channel: {self.channel}", '\n')
+        while not self.target_sta['found']:
+            sniff(prn=self.handle_request,
+                filter=f"udp and ether dst host ff:ff:ff:ff:ff:ff and port {self.usage['port']}",
+                iface="lo",
+                count=1)
         self.broadcast_thread.join()
+        self.target_sta['wait_for'] = True
+
+        # Probe Response
+        while self.target_sta['wait_for']:
+            self.send_probe_response()
+            sniff(prn=self.handle_authentication_request,
+                  filter=f"udp and ether dst host ff:ff:ff:ff:ff:ff and port {self.usage['port']}",
+                  iface="lo",
+                  timeout=1, # timer
+                  count=1)
+        self.target_sta['wait_for'] = True
+
+        # Authentication Response
+        while self.target_sta['wait_for']:
+            self.send_authentication_response()
+            sniff(prn=self.handle_association_requset,
+                  filter=f"udp and ether dst host ff:ff:ff:ff:ff:ff and port {self.usage['port']}",
+                  iface="lo",
+                  timeout=1, # timer
+                  count=1)
+        self.target_sta['wait_for'] = True
+
+        # Association Response
+        while self.target_sta['wait_for']:
+            self.send_association_response()
+            sniff(prn=self.wait_for_eap_tls,
+                  filter=f"ether dst host ff:ff:ff:ff:ff:ff and port {self.usage['port']}",
+                  iface="lo",
+                  timeout=1, # timer
+                  count=1)
+        self.target_sta['wait_for'] = True
+
+        # Start EAP process with target STA
+        self.eap_tls()
 
     def broadcast_beacon(self, interval: float):
         # Send boardcasting packet
-        while True:
+        while self.broadcasting:
             packet = Ether(dst="ff:ff:ff:ff:ff:ff") /\
                      IP(dst="255.255.255.255") /\
-                     UDP(dport=self.dest_port, sport=self.port) /\
-                     Raw(load="Beacon")
+                     UDP(dport=self.usage['dst_port'], sport=self.usage['port']) /\
+                     Raw(load=" | ".join(["frame_type: Beacon",
+                                          f"port: {self.usage['port']}",
+                                          f"dst_port: {self.usage['dst_port']}"]))
             sendp(packet, iface="lo", verbose=False)
-            print("AP broadcast:", packet, '\n')
+            print(f"AP broadcast(seq:{self.usage['seq_num']}):", packet, '\n')
+            self.usage['seq_num'] += 1
             time.sleep(interval) # Broadcast interval
+       
+    def handle_request(self, pkt):
+        if pkt.haslayer(Raw): 
+            try:
+                payload = decode_payload(pkt)
+                if payload.get('frame_type', '') == 'Request':
+                    print("Received Request Packet:", payload, '\n')
+                    self.target_sta['found'] = True
+                    self.target_sta['addr'] = payload['addr']
+                    self.broadcasting = False
+            except Exception as e:
+                print(f"*** Error Processing Request Packet: {e}", '\n')
+                sys.exit(1)
 
-    def handle_signal(self):
-        print("(handle_signal)", '\n')
+    def send_probe_response(self):
+        packet = Ether(dst="ff:ff:ff:ff:ff:ff") /\
+                 IP(dst="255.255.255.255") /\
+                 UDP(dport=self.usage['dst_port'], sport=self.usage['port']) /\
+                 Raw(load=" | ".join(["frame_type: Response",
+                                     f"dst: {self.target_sta['addr']}"]+ 
+                                     [f"{k}: {self.usage[k]}" for k in self.usage]))
+        sendp(packet, iface="lo", verbose=False)
+        print(f"AP send Probe-Response(seq:{self.usage['seq_num']}):", packet, '\n')
+        self.usage['seq_num'] += 1
+
+    def handle_authentication_request(self, pkt):
+        if pkt.haslayer(Raw):
+            try:
+                payload = decode_payload(pkt)
+                if int(payload.get('STA_CHANNEL', -1)) == self.channel and \
+                   payload.get('SSID', '') == self.usage['AP_SSID'] and \
+                   payload.get('frame_type', '') == 'Request':
+                    print("Received Authentication Request Packet:", payload, '\n')
+                    self.target_sta['wait_for'] = False
+                    self.target_sta['seq_num'] = payload['seq_num']
+                    self.target_sta['SSID'] = payload['STA_SSID']
+            except Exception as e:
+                print(f"*** Error Processing Authentication Packet: {e}", '\n')
+                sys.exit(1)
+
+    def send_authentication_response(self):
+        packet = Ether(dst="ff:ff:ff:ff:ff:ff") /\
+                 IP(dst="255.255.255.255") /\
+                 UDP(dport=self.usage['dst_port'], sport=self.usage['port']) /\
+                 Raw(load=" | ".join(["frame_type: Response",
+                                     f"dst: {self.target_sta['addr']}",
+                                     f"response: {int(True)}"]+ 
+                                     [f"{k}: {self.usage[k]}" for k in self.usage]))
+        sendp(packet, iface="lo", verbose=False)
+        print(f"AP send Authentication-Response(seq:{self.usage['seq_num']}):", packet, '\n')
+        self.usage['seq_num'] += 1
+
+    def handle_association_requset(self, pkt):
+        if pkt.haslayer(Raw):
+            try:
+                payload = decode_payload(pkt)
+                if payload.get('frame_type', '') == 'Request' and \
+                   payload.get('STA_SSID', '') == self.target_sta['SSID'] and \
+                   self.target_sta['seq_num'] != payload['seq_num']:
+                    print("Received Association Request Packet:", payload, '\n')
+                    self.target_sta['wait_for'] = False
+                    self.target_sta['seq_num'] = payload['seq_num']
+            except Exception as e:
+                print(f"*** Error Processing Association Packet: {e}", '\n')
+                sys.exit(1)
+
+    def send_association_response(self):
+        packet = Ether(dst="ff:ff:ff:ff:ff:ff") /\
+                 IP(dst="255.255.255.255") /\
+                 UDP(dport=self.usage['dst_port'], sport=self.usage['port']) /\
+                 Raw(load=" | ".join(["frame_type: Response",
+                                     f"dst: {self.target_sta['addr']}",
+                                     f"response: {int(True)}",
+                                     f"assosiation: DONE"]+ 
+                                     [f"{k}: {self.usage[k]}" for k in self.usage]))
+        sendp(packet, iface="lo", verbose=False)
+        print(f"AP send Association-Response(seq:{self.usage['seq_num']}):", packet, '\n')
+        self.usage['seq_num'] += 1
+
+    def wait_for_eap_tls(self, pkt):
+        if pkt.haslayer(Raw):#
+            try:
+                payload = decode_payload(pkt)
+                if payload.get('frame_type', '') == self.security and \
+                   payload.get('STA_SSID', '') == self.target_sta['SSID'] and \
+                   payload.get('code', '') == 'START' and \
+                   self.target_sta['seq_num'] != payload['seq_num']:
+                    print("Received EAP-START Packet:", payload, '\n')
+                    self.target_sta['wait_for'] = False
+                    self.target_sta['seq_num'] = payload['seq_num']
+            except Exception as e:
+                print(f"*** Error Processing EAPOL-START Packet: {e}", '\n')
+                sys.exit(1)
+
+    def eap_tls(self):
+        # ...
+        print("AP Ready for EAP_TLS ...", '\n')
         pass
 
-if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print("Usage: EAP_AP <PORT_NUMBER> <DEST_PORT_NUMBER>", '\n')
-        sys.exit(1)
 
-    try:
-        argv_port = int(sys.argv[1])
-        argv_dest_port = int(sys.argv[2])
-    except ValueError:
-        print("Error: integer <PORT_NUMBER> <DEST_PORT_NUMBER>", '\n')
-        sys.exit(1)
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: EAP_AP <CHANNEL_NUMBER>", '\n')
+    else:
+        try:
+            argv_channel = int(sys.argv[1])
+        except Exception as e:
+            argv_channel = 1
+            print("Error: integer <CHANNEL_NUMBER>")
+            print(f"Usage: CHANNEL_NUMBER-{argv_channel}", '\n')
 
     # Wifi-AP simulation on loopback interface
-    AP = EAP_AP(port=argv_port, dest_port=argv_dest_port) 
+    AP = EAP_AP(channel=argv_channel)
     AP.start()
--- EAP_STA.py
+++ EAP_STA.py
@@ -2,49 +2,187 @@ import sys
 # import socket
 from scapy.all import *
 
+def decode_payload(pkt):
+    return {i.split(':')[0].strip():i.split(':')[1].strip() for i in [p.strip() for p in pkt[Raw].load.decode('utf-8').split('|')]}
+
 class EAP_STA():
-    def __init__(self, port: int, dest_port: int) -> None:
-        self.port = port
-        self.dest_port = dest_port
+    def __init__(self, security:str = 'EAP-TLS') -> None:
+        self.security = security
+        self.usage = dict()
+        self.target_ap = {
+            'found': False,
+            'wait_for': False,
+        }
         self.setup()
 
     def setup(self):
-        print(f"STA<{get_if_addr(conf.iface)}:{self.port}> Setup", '\n')
+        self.usage = {
+            'seq_num': 0,
+            # WIFI (simulated)
+            'STA_SSID': "EAP_STA",
+            'STA_CHANNEL': None,
+            # ETHERNET
+            'port': None,
+            'dst_port': None,
+        }
+        print(f"STA<{get_if_addr(conf.iface)}> Setup", '\n')
 
     def start(self):
-        print("STA started listening for beacon frames...", '\n')
-        while True:
-            sniff(prn=self.handle_beacon,
-                  filter=f"udp and port {self.port}",
-                  iface="lo")
+        print("STA started listening for beacon frames ...", '\n')
+        while not self.target_ap['found']:
+            self.sniff_beacon()
+        self.target_ap['wait_for'] = True
+
+        # Probe Request
+        while self.target_ap['wait_for']:
+            self.send_probe_request()
+            sniff(prn=self.handle_probe_response,
+                  filter=f"udp and ether dst host ff:ff:ff:ff:ff:ff and port {self.usage['port']}",
+                  iface="lo",
+                  timeout=1, # timer
+                  count=1)
+        self.target_ap['wait_for'] = True
+
+        # Authentication Request
+        while self.target_ap['wait_for']:
+            self.send_authentication_request()
+            sniff(prn=self.handle_authentication_response,
+                  filter=f"udp and ether dst host ff:ff:ff:ff:ff:ff and port {self.usage['port']}",
+                  iface="lo",
+                  timeout=1, # timer
+                  count=1)
+        self.target_ap['wait_for'] = True
+
+        # Association Request
+        while self.target_ap['wait_for']:
+            self.send_association_requset()
+            sniff(prn=self.handle_authentication_response,
+                  filter=f"udp and ether dst host ff:ff:ff:ff:ff:ff and port {self.usage['port']}",
+                  iface="lo",
+                  timeout=1, # timer
+                  count=1)
+        self.target_ap['wait_for'] = True
+
+        # Start EAP process with target AP
+        self.eap_tls()
+
+    def sniff_beacon(self):
+        sniff(prn=self.handle_beacon,
+                  filter=f"udp and ether dst host ff:ff:ff:ff:ff:ff",
+                  iface="lo",
+                  count=1)
 
     def handle_beacon(self, pkt):
-        print(f"Received Packet Details: dest{pkt.dst}")
-        if pkt.haslayer(Raw): print(pkt[Raw].load)
-        print()
-        # pkt.show()
-        
+        if pkt.haslayer(Raw): 
+            try:
+                payload = decode_payload(pkt)
+                if payload.get('frame_type', '') == 'Beacon':
+                    print("Received Beacon:", payload, '\n')
+                    self.target_ap['found'] = True
+                    self.usage['port'] = int(payload['dst_port'])
+                    self.usage['dst_port'] = int(payload['port'])
+            except Exception as e:
+                print(f"*** Error Processing Beacon Packet: {e}", '\n')
+                sys.exit(1)
 
-    # def handle_beacon(self, pkt):
-    #     if pkt.haslayer(Raw) and pkt[Raw].load == "Beacon":
-    #         print("Received Beacon Frame from AP:")
-    #         print(pkt.summary())  # Print a summary of the received packet
-            
-            # Process Frame
-            # ...
+    def send_probe_request(self):
+        # Create a probe request packet
+        packet = Ether(dst="ff:ff:ff:ff:ff:ff") /\
+                 IP(dst="255.255.255.255") /\
+                 UDP(dport=self.usage['dst_port'], sport=self.usage['port']) /\
+                 Raw(load=" | ".join(["frame_type: Request",
+                                      f"addr: {get_if_addr(conf.iface)}"]))
+        sendp(packet, iface="lo", verbose=False)
+        print(f"STA send Probe-Request(seq:{self.usage['seq_num']}):", packet, '\n')
+        self.usage['seq_num'] += 1
+
+    def handle_probe_response(self, pkt):
+        if pkt.haslayer(Raw): 
+            try:
+                payload = decode_payload(pkt)
+                if payload.get('dst', '') == get_if_addr(conf.iface) and \
+                   payload.get('frame_type', '') == 'Response':
+                    print(f"Received Response: {payload}", '\n')
+                    self.target_ap['wait_for'] = False
+                    self.target_ap['seq_num'] = payload['seq_num']
+                    self.target_ap['SSID'] = payload['AP_SSID']
+                    self.target_ap['RATE'] = payload['AP_RATE']
+                    self.usage['STA_CHANNEL'] = payload['AP_CHANNEL']
+            except Exception as e:
+                print(f"*** Error Processing Response Packet: {e}", '\n')
+                sys.exit(1)
+
+    def send_authentication_request(self):
+        # Create a authentication request packet
+        packet = Ether(dst="ff:ff:ff:ff:ff:ff") /\
+                 IP(dst="255.255.255.255") /\
+                 UDP(dport=self.usage['dst_port'], sport=self.usage['port']) /\
+                 Raw(load=" | ".join(["frame_type: Request", f"SSID: {self.target_ap['SSID']}"]+
+                                     [f"{k}: {self.usage[k]}" for k in self.usage]))
+        sendp(packet, iface="lo", verbose=False)
+        print(f"STA send Authentication-Request(seq:{self.usage['seq_num']}):", packet, '\n')
+        self.usage['seq_num'] += 1
+
+    def handle_authentication_response(self, pkt):
+        if pkt.haslayer(Raw): 
+            try:
+                payload = decode_payload(pkt)
+                if payload.get('AP_SSID', '') == self.target_ap['SSID'] and \
+                   payload.get('dst', '') == get_if_addr(conf.iface) and \
+                   payload.get('frame_type', '') == 'Response' and int(payload.get('response')) and \
+                   self.target_ap['seq_num'] != payload['seq_num']:
+                    print(f"Received Authentication Response: {payload}", '\n')
+                    self.target_ap['wait_for'] = False
+                    self.target_ap['seq_num'] = payload['seq_num']
+            except Exception as e:
+                print(f"*** Error Processing Authentication Packet: {e}", '\n')
+                sys.exit(1)
+
+    def send_association_requset(self):
+        # Create a association request packet
+        packet = Ether(dst="ff:ff:ff:ff:ff:ff") /\
+                 IP(dst="255.255.255.255") /\
+                 UDP(dport=self.usage['dst_port'], sport=self.usage['port']) /\
+                 Raw(load=" | ".join(["frame_type: Request",
+                                      f"rsn: {self.security}"]+
+                                      [f"{k}: {self.usage[k]}" for k in self.usage]))
+        sendp(packet, iface="lo", verbose=False)
+        print(f"STA send Association-Request(seq:{self.usage['seq_num']}):", packet, '\n')
+        self.usage['seq_num'] += 1
+
+    def handle_association_response(self, pkt):
+        if pkt.haslayer(Raw): 
+            try:
+                payload = decode_payload(pkt)
+                if payload.get('assosiation', '') == 'DONE' and \
+                   payload.get('AP_SSID', '') == self.target_ap['SSID'] and \
+                   payload.get('dst', '') == get_if_addr(conf.iface) and \
+                   payload.get('frame_type', '') == 'Response' and int(payload.get('response')) and \
+                   self.target_ap['seq_num'] != payload['seq_num']:
+                    print(f"Received Association Response: {payload}", '\n')
+                    self.target_ap['wait_for'] = False
+                    self.target_ap['seq_num'] = payload['seq_num']
+            except Exception as e:
+                print(f"*** Error Processing Association Packet: {e}", '\n')
+                sys.exit(1)
+
+    def eap_tls(self):
+        print(f'Start EAP-TLS with AP(ssid:{self.target_ap["SSID"]})', '\n')
+        
+        # EAP-Start
+        packet = Ether(dst="ff:ff:ff:ff:ff:ff") /\
+                 IP(dst="255.255.255.255") /\
+                 TCP(dport=self.usage['dst_port'], sport=self.usage['port']) /\
+                 Raw(load=" | ".join([f"frame_type: {self.security}",
+                                      f"code: START"]+
+                                      [f"{k}: {self.usage[k]}" for k in self.usage]))
+        sendp(packet, iface="lo", verbose=False)
+        print(f"STA send EAPOL-START(seq:{self.usage['seq_num']}):", packet, '\n')
+        self.usage['seq_num'] += 1
+
+        # ...
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print("Usage: EAP_AP <PORT_NUMBER> <DEST_PORT_NUMBER>", '\n')
-        sys.exit(1)
-
-    try:
-        argv_port = int(sys.argv[1])
-        argv_dest_port = int(sys.argv[2])
-    except ValueError:
-        print("Error: integer <PORT_NUMBER> <DEST_PORT_NUMBER>", '\n')
-        sys.exit(1)
-    
     # Wifi-STA simulation on loopback interface
-    STA = EAP_STA(port=argv_port, dest_port=argv_dest_port) # loopback ip
+    STA = EAP_STA()
     STA.start()
